eec0bb6f00e4b2f2da729479cd399e0619d19249
PeterAJansen <pajansen@email.arizona.edu>
2021-11-30 17:55:51 -0700

    small additions

--- software/teensy/serialTest.py
+++ software/teensy/serialTest.py
@@ -7,6 +7,26 @@ s = serial.Serial("/dev/ttyACM0", 115200)
 
 s.write(b"Hello World")
 
+# Wait for 'data'
+for i in range(0, 10000):
+    line = s.readline()
+    line = line.decode('UTF-8').strip()    
+    if (line.startswith("DATA:")):
+        break
+
+data = []
 for i in range(0, 1000):
     line = s.readline()   # read a '\n' terminated line
-    print(line)
+    line = line.decode('UTF-8').strip()    
+
+    value = -1
+    try:
+        value = int(line)        
+        data.append(value)
+    except ValueError:
+        # do nothing
+        value = -1
+
+
+for i in range(0, len(data)):
+    print(str(i) + "\t" + str(data[i]))
