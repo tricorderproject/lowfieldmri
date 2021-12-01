3fcd3c6ffa6a1dd170bee1ad677d9d06a7b17c57
PeterAJansen <pajansen@email.arizona.edu>
2021-11-30 18:00:41 -0700

    added simple matplotlib plot to radio data coming in

--- software/teensy/serialTest.py
+++ software/teensy/serialTest.py
@@ -2,31 +2,38 @@
 
 
 import serial
+import matplotlib.pyplot as plt
 
 s = serial.Serial("/dev/ttyACM0", 115200)
 
 s.write(b"Hello World")
 
-# Wait for 'data'
-for i in range(0, 10000):
-    line = s.readline()
-    line = line.decode('UTF-8').strip()    
-    if (line.startswith("DATA:")):
-        break
+for a in range(0, 10):
+    # Wait for 'data'
+    for i in range(0, 10000):
+        line = s.readline()
+        line = line.decode('UTF-8').strip()    
+        if (line.startswith("DATA:")):
+            break
 
-data = []
-for i in range(0, 1000):
-    line = s.readline()   # read a '\n' terminated line
-    line = line.decode('UTF-8').strip()    
+    data = []
+    for i in range(0, 1000):
+        line = s.readline()   # read a '\n' terminated line
+        line = line.decode('UTF-8').strip()    
 
-    value = -1
-    try:
-        value = int(line)        
-        data.append(value)
-    except ValueError:
-        # do nothing
         value = -1
+        try:
+            value = int(line)        
+            data.append(value)
+        except ValueError:
+            # do nothing
+            value = -1
 
 
-for i in range(0, len(data)):
-    print(str(i) + "\t" + str(data[i]))
+    for i in range(0, len(data)):
+        print(str(i) + "\t" + str(data[i]))
+
+    plt.clf()
+    plt.plot(data)
+    plt.draw()
+    plt.pause(0.001)
